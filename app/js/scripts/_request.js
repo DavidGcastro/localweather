d32a89cbabb18b778a2e4a22e5f00f86e5cd40ab
David <davidgcastro93@gmail.com>
2018-01-23 18:08:29 -0500

    added more info

--- app/js/scripts/_request.js
+++ app/js/scripts/_request.js
@@ -1,6 +1,9 @@
 var city = document.getElementById("city");
 var temp = document.getElementById("temp");
 var icon = document.getElementById("icon")
+var minTemp = document.getElementById("minTemp");
+var maxTemp = document.getElementById("maxTemp");
+var windSpeed = document.getElementById("windSpeed");
 var weatherText = document.getElementById("weatherText");
 
 
@@ -16,8 +19,12 @@ if ("geolocation" in navigator) {
 
 
 function cords(latitude, longitude) {
+
+    console.log(latitude);
+    console.log(longitude);
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://fcc-weather-api.glitch.me//api/current?lat=' + latitude + '&lon=' + longitude)
+    ourRequest.open('GET', 'https://fcc-weather-api.glitch.me//api/current?lon=' + longitude + '&lat=' + latitude)
+
     ourRequest.send();
 
     //when data loads
@@ -28,13 +35,19 @@ function cords(latitude, longitude) {
 
         if (data.sys.country === "US") {
             temp.innerHTML = toF(data.main.temp);
+            maxTemp.innerHTML = toF(data.main.temp_max);
+            minTemp.innerHTML = toF(data.main.temp_min);
+
         } else {
             temp.innerHTML = (data.main.temp) + " C\xB0";
-
+            maxTemp.innerHTML = data.main.temp_max;
+            minTemp.innerHTML = data.main.temp_min;
         }
 
         city.innerHTML = (data.name);
         weatherText.innerHTML = (data.weather[0].description);
+        //NEEDS TO BE CONVERTED TO KPH MAKE A FUNCTION
+        windSpeed.innerHTML = data.wind.speed + " MPH";
 
         //manipulating the icon
 
@@ -50,23 +63,24 @@ function cords(latitude, longitude) {
             icon.src = "images/SVG/Cloud-Drizzle.svg";
 
         }
-        if (data.weather[0].description == "clear sky") {
-            icon.src = "images/SVG/Sun.svg";
 
-        }
-        if (data.weather[0].description == "clear sky") {
-            icon.src = "images/SVG/Sun.svg";
-
-        }
-        if (data.weather[0].description == "clear sky") {
-            icon.src = "images/SVG/Sun.svg";
-
-        }
-        if (data.weather[0].description == "clear sky") {
-            icon.src = "images/SVG/Sun.svg";
-
-        }
-
+        // add more icons here: 
+        //        if (data.weather[0].description == "") {
+        //            icon.src = "images/SVG/.svg";
+        //
+        //        }
+        //        if (data.weather[0].description == "") {
+        //            icon.src = "images/SVG/.svg";
+        //
+        //        }
+        //        if (data.weather[0].description == " {
+        //            icon.src = "images/SVG/.svg";
+        //
+        //        }
+        //        if (data.weather[0].description == "") {
+        //            icon.src = "images/SVG/.svg";
+        //
+        //        }
     };
 
 
